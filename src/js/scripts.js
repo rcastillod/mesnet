2fc4fc2a67e1fe4b4301e1fd222c1e2a252153ca
rcastillod <rodrigo.castillo@global3.cl>
2023-03-07 23:03:14 -0300

    feat: :construction: Working in plans tabs

---
+++ src/js/scripts.js
@@ -0,0 +1,10 @@
+/* -------------------------------------------------------------------------- */
+/*                              Custom icon tabs                              */
+/* -------------------------------------------------------------------------- */
+
+// Tab 1
+const tab1 = document.getElementById('elementor-tab-title-1581')
+const tab1Img = document.createElement('img')
+tab1Img.src = '//localhost:9050/wp-content/uploads/2023/03/isotipo-mesnet.svg'
+
+tab1.appendChild(tab1Img)
